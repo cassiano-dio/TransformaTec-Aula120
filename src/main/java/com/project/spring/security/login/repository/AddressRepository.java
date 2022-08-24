70a4f456edcd4fd82aabc927406b25a2d2e2b677
Cassiano Ricardo de Oliveira Peres <97028976+cassiano-dio@users.noreply.github.com>
2022-08-24 14:06:18 -0300

    Delete AddressRepository.java

--- src/main/java/com/project/spring/security/login/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.spring.security.login.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.spring.security.login.models.Address;
-
-public interface AddressRepository extends JpaRepository<Address, Long>{
-    
-}
